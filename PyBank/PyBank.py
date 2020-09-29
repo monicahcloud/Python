27c87e7ec57cc0544a15f8363d1b52bd5a491e23
Monicah Cloud <monicahcloud@gmail.com>
2020-09-28 23:50:37 -0400

    PyBank

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -3,7 +3,10 @@
 import os
 import csv 
 
-bankpath=os.path.join('..','Resources','Budget_Data.csv')
+bankpath=os.path.join('Resources','Budget_Data.csv')
+
+analysis_file = os.path.join('Analysis','Cloud_analysis_output.txt')
+analysis_output = open(analysis_file, 'w')
 
 with open(bankpath) as bankfile:
     bankreader = csv.reader(bankfile, delimiter=',')
@@ -14,19 +17,15 @@ with open(bankpath) as bankfile:
     rev = []
     revenue_change = []
     monthly_change = []
-    
-    #print(f"Header: {bank_header}")               
-
+     #print(f"Header: {bank_header}")               
 #Months       
     for row in bankreader:
         month.append(row[0])
         rev.append(row[1])
-    #print(len(month))
-    
+  
  #Revenue 
     rev_int = map(int,rev)
     total_revenue = (sum(rev_int))
-    #print(total_revenue)
 
  #Avg Change
     i = 0
@@ -35,32 +34,37 @@ with open(bankpath) as bankfile:
  # append profit_loss
         revenue_change.append(profit_loss)
     Total = sum(revenue_change)
-    #print(revenue_change)
+   
     monthly_change = Total / len(revenue_change)
-    #print(monthly_change)
-    #print(Total)
     
 #Greatest Increase
     profit_increase = max(revenue_change)
-    #print(profit_increase)
+ 
     profitgains = revenue_change.index(profit_increase)
     month_increase = month[profitgains+1]
     
 #Greatest Decrease
     profit_decrease = min(revenue_change)
-    #print(profit_decrease)
+   
     profitloss = revenue_change.index(profit_decrease)
     month_decrease = month[profitloss+1]
 
-#Print Statements
-output(
-print(f'Financial Analysis')
-print("Total Months: " + str(len(month)))
-print("Total Revenue: $ " + str(total_revenue)) 
-print("Average monthly change: $" + str(monthly_change))
-print(f"Greatest Increase in Profits: {month_increase} (${profit_increase})")
-)
+Avg_total = round(float(monthly_change),2)
+total_month = len(month)
 
-#Write to the text path
-with open(pathout, "w") as txt_file:
-    txt_file.write(output)
+#Print Statements
+
+print(f'Financial Analysis')
+analysis_output.write('Financial Analysis \n')
+print(f"---------------------------")
+analysis_output.write('---------------------------')
+print(f'Total Months:   {total_month} \n')
+analysis_output.write(f'\nTotal Months: {total_month} \n')
+print(f"Total Revenue: $ " + str(total_revenue)) 
+analysis_output.write(f'Total Revenue: {total_revenue} \n')
+print(f"Average monthly change: $" + str(Avg_total))
+analysis_output.write(f'Average monthly change: $ {Avg_total} \n')
+print(f"Greatest Increase in Profits: {month_increase} (${profit_increase})")
+analysis_output.write(f'Greatest Increase in Profits: {month_increase} (${profit_increase}) \n')
+print(f'Greatest Decrease in Revenue: {month_decrease} (${profit_decrease})')
+analysis_output.write(f'Greatest Decrease in Revenue: {month_decrease} (${profit_decrease}) \n')
